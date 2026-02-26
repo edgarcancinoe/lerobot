f38960f41131239df7c23a2e0f53b36eeaf1c411
Edgar <edgarcancinoe@gmail.com>
2026-02-26 21:04:55 +0100

    a ver

--- src/lerobot/scripts/lerobot_train.py
+++ src/lerobot/scripts/lerobot_train.py
@@ -324,7 +324,6 @@ def train(cfg: TrainPipelineConfig, accelerator: Accelerator | None = None):
                 "norm_map": policy.config.normalization_mapping,
             },
         }
-    print(cfg.policy)
     preprocessor, postprocessor = make_pre_post_processors(
         policy_cfg=cfg.policy,
         pretrained_path=cfg.policy.pretrained_path,
